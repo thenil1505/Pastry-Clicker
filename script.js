998a4d172859f096dcc63682e04f4a503a2bbe83
thenil1505 <thenil@vgs.nfk.no>
2022-10-17 09:59:59 +0200

    Oh yeah
    Trying to add the buildings into the game, adding code corresponding to the bakery

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ pastry_num_EL = document.querySelector("#pastry_num");
 hand_EL = document.querySelector("#hand_img");
 hand_num_EL = document.querySelector("#hand_num");
 hand_price_EL = document.querySelector("#hand_price");
+bakeri_EL = document.querySelector("#bakeri_pic");
 
 console.log(pastry_num_EL);
 
@@ -47,12 +48,20 @@ function buy_hand(){
         hands_price += 50;
         hand_num_EL.innerHTML = hands;
         hand_price_EL.innerHTML = hands_price;
-    }
-    
+    } 
+}
+
+function buy_bake(){
+    if (pastry >= bakery_price){
+        bakeries++;
+        pastry -= bakery_price;
+        bakeri_EL.innerHTML += <img src="photos pastry/bakery.jpg" alt=""></img>
+   }
 }
 
 pastry_EL.addEventListener("click", pastry_click); // når vi klikker på pastry
 hand_EL.addEventListener("click", buy_hand)
+bakeri_EL.addEventListener("click", buy_bake)
 
 
 var timer = setInterval(myTimer, 60); // kjører funksjonen myTimer 1 gang i sekundet
